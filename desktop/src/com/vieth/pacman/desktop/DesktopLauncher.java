52a0ada2d54633f37ae3850c0deeea77f9c6f940
Bastian-Lahrmann <76053834+Bastian-Lahrmann@users.noreply.github.com>
2020-12-22 20:33:25 +0100

    Create DesktopLauncher.java

--- desktop/src/com/vieth/pacman/desktop/DesktopLauncher.java
+++ desktop/src/com/vieth/pacman/desktop/DesktopLauncher.java
@@ -5,8 +5,8 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.vieth.pacman.PacMan;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new PacMan(), config);
-	}
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new PacMan(), config);
+    }
 }

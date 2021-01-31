003cd8df0394cc1943d261d0c909feec268186c2
Bastian-Lahrmann <76053834+Bastian-Lahrmann@users.noreply.github.com>
2021-01-31 04:21:49 +0100

    customized pause-screen

--- core/src/uas/lntv/pacmangame/Screens/PauseScreen.java
+++ core/src/uas/lntv/pacmangame/Screens/PauseScreen.java
@@ -1,7 +1,14 @@
 package uas.lntv.pacmangame.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import uas.lntv.pacmangame.PacManGame;
 import uas.lntv.pacmangame.Scenes.Hud;
@@ -12,15 +19,18 @@ public class PauseScreen extends MapScreen {
     private GameScreen screen;
     private BitmapFont font;
 
+
     public PauseScreen(PacManGame game, String mapPath, GameScreen screen){
         super(game, mapPath, Type.MENU);
         this.pacman = new PacMan(game, 25*map.tileSize, 47*map.tileSize, this, hud);
-        this.pacman.setSpeed(16);
+        this.pacman.setSpeed(8);
         this.ghost = new Enemy(25*map.tileSize, 3*map.tileSize, this, Enemy.Difficulty.EASY);
         this.hud = new Hud(game, this, false);
         this.font = new BitmapFont();
         font.getData().setScale(font.getScaleX()*2);
         this.screen = screen;
+        music.pause();
+
     }
 
     @Override
@@ -28,20 +38,40 @@ public class PauseScreen extends MapScreen {
         super.render(delta);
 
         game.batch.begin();
-        for(int i = 0; i < 10; i++) {
-            font.draw(
-                    game.batch,
-                    game.highScore.getHighScores().get(i).toString(),
-                    map.tileSize * 3,
-                    (map.tileSize * 44)-3*map.tileSize*i
-            );
-        }
+            font.draw(game.batch, "CONTINUE", 2 * map.tileSize,48* map.tileSize);
+            font.draw(game.batch, "RETURN TO MENU", 2 * map.tileSize,17* map.tileSize);
+            font.draw(game.batch, "MUSIC", 11 * map.tileSize + 20,43* map.tileSize);
+            font.draw(game.batch, "ON", 10 * map.tileSize,38* map.tileSize);
+            font.draw(game.batch, "OFF", 15 * map.tileSize,38* map.tileSize);
         game.batch.end();
 
-        if(pacman.getXPosition() <= 2*map.tileSize) {
-            game.setScreen(screen);
-            this.dispose();
+        //RETURN
+        if(pacman.getYPosition() == 47*map.tileSize){
+            if(pacman.getXPosition() <= 2*map.tileSize) {
+                game.setScreen(screen);
+                this.dispose();
+            }
         }
+        //TO MENU
+        if(pacman.getYPosition() == 16*map.tileSize){
+            if(pacman.getXPosition() <= 2*map.tileSize) {
+                game.setScreen(new MenuScreen(game, "mainMenu.tmx"));
+                this.dispose();
+            }
+
+        }
+        //MUSIC
+        if(pacman.getYPosition() == 37*map.tileSize){
+            if(pacman.getXPosition() == 10*map.tileSize) {
+                // TO BE MADE
+                
+            }
+            if(pacman.getXPosition() == 15*map.tileSize) {
+                // TO BE MADE
+
+            }
+        }
+
     }
 
 }

600bf1232db78f58ae4aa0adae313e28940621f6
CruseoGithub <42396631+CruseoGithub@users.noreply.github.com>
2021-03-15 10:29:17 +0100

    Doku: Map
    Doku zu Map, MenuMap, GameMap

--- core/src/uas/lntv/pacmangame/Maps/GameMap.java
+++ core/src/uas/lntv/pacmangame/Maps/GameMap.java
@@ -8,10 +8,22 @@ import uas.lntv.pacmangame.PacManGame;
 import uas.lntv.pacmangame.Screens.MapScreen;
 import uas.lntv.pacmangame.Sprites.Enemy;
 
+/**
+ * this class sets up a map for a game level.
+ * It implements some additional code for generating and collecting collectables
+ */
 public class GameMap extends Map {
     MapScreen screen;
     private final PacManGame GAME;
 
+    /**
+     * does the same as the parent constructor.
+     * Additionaly it generates collectables and provides a method to collect them.
+     * @param game instance of PacManGame
+     * @param assets instance of the Assetmanager
+     * @param path  string value which contains the path to a tmx-Mapfile.
+     * @param screen instance of a Screen which contains this map
+     */
     public GameMap(PacManGame game, Assets assets, String path, MapScreen screen){
         super(path, assets);
         this.GAME = game;
@@ -20,6 +32,9 @@ public class GameMap extends Map {
         generateDots(150);
     }
 
+    /**
+     * generates hunter items
+     */
     public void generateItems(){
         for(int x = 0; x < MAP_WIDTH; x++){
             for(int y = 0; y < MAP_HEIGHT; y++){
@@ -30,6 +45,11 @@ public class GameMap extends Map {
         }
     }
 
+    /**
+     * generates all simple dots/scorepoints which can be collected by Pac-Man.
+     * It does this by iterating through the tile matrix and placing items by chance (default: 50% chance) until it reaches a total amount of items.
+     * @param total_Dots the total amount of Dots/Points generated on the map
+     */
     public void generateDots(int total_Dots){
         while(total_Dots > 0){
             for(int x = 0; x < MAP_WIDTH; x++){
@@ -54,6 +74,12 @@ public class GameMap extends Map {
         }
     }
 
+    /**
+     * this will delete a collectable from the map and plays a sound
+     * additionaly it will in increase Pac-Mans score value.
+     * If the collectable is a hunter item it will evolve Pac-Man to SuperPacMan and set the ghosts to a freightened state.
+     * @param tile specify the tile from which you want to collect an item
+     */
     public void collect(Tile tile){
         if(tile.isItem()){
             layerCollect.setCell(
--- core/src/uas/lntv/pacmangame/Maps/MenuMap.java
+++ core/src/uas/lntv/pacmangame/Maps/MenuMap.java
@@ -2,13 +2,26 @@ package uas.lntv.pacmangame.Maps;
 
 import uas.lntv.pacmangame.Assets;
 
+/**
+ * This class provides a simple map for the purpose of creating menus.
+ * It will not create collectables besides the ones already provided by the tmx-mapfile
+ */
 public class MenuMap extends Map {
 
+    /**
+     * Does the same as the parent constructor, but creates no additional collectables.
+     * @param path String value which contains the path to a tmx-Mapfile.
+     * @param assets provide the Assetsmanager instance of the game.
+     */
     public MenuMap(String path, Assets assets){
         super(path, assets);
         generateDots(0);
     }
 
+    /**
+     * searches for collectables in the tmx-mapfile layer and adds them to the tile matrix
+     * @param total_Dots the total amount of Dots/Points generated on the map
+     */
     @Override
     public void generateDots(int total_Dots) {
         for(int x = 0; x < MAP_WIDTH; x++){
@@ -20,6 +33,10 @@ public class MenuMap extends Map {
         }
     }
 
+    /**
+     * Does the same as the parent constructor.
+     * @param tile specify the tile from which you want to collect an item
+     */
     @Override
     public void collect(Tile tile) {
         super.collect(tile);

aecb292941ac19238e3c22626ba8b28bbecb897e
Newton-76 <76390647+Newton-76@users.noreply.github.com>
2021-03-17 13:27:36 +0100

    pause in buttons

--- core/src/uas/lntv/pacmangame/Scenes/ControllerButtons.java
+++ core/src/uas/lntv/pacmangame/Scenes/ControllerButtons.java
@@ -18,6 +18,7 @@ public class ControllerButtons extends Controller {
                 Vector3 touch = new Vector3(screenX, screenY, 0);
                 gameCam.unproject(touch);
 
+                ready(touch.x, touch.y);
                 System.out.println("Screen coordinates Buttons!!!!!!!: "
                         + "X: " + touch.x + " Y: " + touch.y);
 
@@ -35,13 +36,11 @@ public class ControllerButtons extends Controller {
 
             @Override
             public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-                if(isLeftPressed()) leftPressed = false;
-                if(isRightPressed()) rightPressed = false;
-                if(isUpPressed()) upPressed = false;
-                if(isDownPressed()) downPressed = false;
+                Vector3 touch = new Vector3(screenX, screenY, 0);
+                gameCam.unproject(touch);
+                setPause(touch.x, touch.y);
                 return super.touchUp(screenX, screenY, pointer, button);
             }
-
         });
     }
 }

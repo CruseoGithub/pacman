d1a008bfd89ed8212c1f37843c842a5e2fa5faab
CruseoGithub <42396631+CruseoGithub@users.noreply.github.com>
2021-03-19 04:35:22 +0100

    splash.tmx

---
+++ android/assets/maps/LNTVLogo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="LNTVLogo" tilewidth="32" tileheight="32" tilecount="414" columns="23">
+ <image source="../setup/LNTVLogo.png" trans="257a7f" width="746" height="598"/>
+</tileset>

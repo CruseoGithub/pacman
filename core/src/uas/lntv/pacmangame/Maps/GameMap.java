73e5cf3b19addd8710bf46d2b699dd800fa5e785
CruseoGithub <42396631+CruseoGithub@users.noreply.github.com>
2021-03-22 18:33:01 +0100

    sloppy golden ratio
    Algorithm that is loosely based on the golden ratio. Priority of items can be changed in class Tile

--- core/src/uas/lntv/pacmangame/Maps/GameMap.java
+++ core/src/uas/lntv/pacmangame/Maps/GameMap.java
@@ -57,6 +57,9 @@ public class GameMap extends Map {
         generateRandomItem();
     }
 
+    /**
+     * Generates an item based on priority/percentage on the map.
+     */
     @Override
     public void generateRandomItem(){
             Tile.Item random = randomItem();
@@ -64,12 +67,86 @@ public class GameMap extends Map {
             matrix[(int)randomItemPos.x][(int)randomItemPos.y].placeItem(random);
     }
 
+    /**
+     * It will generate an array that represents a percentage cake that includes every item based on its specific priority
+     * From this array it will choose a random item.
+     * @return returns a random item based on percentage ( percentage is based on the golden ratio )
+     */
     public Tile.Item randomItem(){
-        int min = 2; // 0 and 1 are not special collectibles: 0 = Empty ; 1 = Dot/Scorepoint
-        int max = Tile.Item.values().length -1;
-        int random = (int) (Math.random() * (max - min + 1) + min); // random ist zwischen 1 und 4
         Tile.Item[] itemList = Tile.Item.values();
-        return itemList[random];
+
+        double[] percentList = getPercentage(Tile.Item.values().length-2);
+        Tile.Item[] percentCake = new Tile.Item[100];
+        int j = 2;
+        for(int i = 0; i < 100; i++){
+            if(percentList[j-2] > 0) {
+                percentCake[i] = itemList[j];
+                percentList[j-2]--;
+            }
+            else j++;
+        }
+
+        int min = 0; // 0 and 1 are not special collectibles: 0 = Empty ; 1 = Dot/Scorepoint
+        int max = percentCake.length-1; //length is 100
+        int random = (int) (Math.random() * (max - min + 1) + min);
+        return percentCake[random];
+    }
+
+    /**
+     * This calculates the golden ratio based on the number of items
+     * @param items total number of items
+     * @return returns the golden ratio
+     */
+    public static double goldenRatio(int items) {
+        if (items == 0) return 1;
+        return 1.0 + 1.0 / goldenRatio(items-1);
+    }
+
+    /**
+     * it calculates a percentage portion for each item based on the golden ration. (it wont amount to exactly 100% though)
+     * Any left over percentages will be destributed equaly to every item
+     * @param items total number of items
+     * @return returns a list of percentages for each item
+     */
+    public static double[] getPercentage(int items) {
+        double ratio = goldenRatio(items);
+
+        //Calculating every percentage portion fore each item
+        double[] percentList = new double[items];
+        double max_old, max_new, percent;
+        max_old = 100.0;
+        for (int i = 0; i < items; i++){
+            max_new = max_old / ratio;
+            percent = max_old - max_new;
+
+            //fractions bigger than one half shall be rounded up to the nearest whole number;
+            //fractions smaller than one half shall be rounded down to the nearest whole number.
+            if((percent-(int)percent) < 0.5) percentList[i] = (int)percent;
+            else percentList[i] = (int)percent + 1;
+            max_old = max_new;
+        }
+        int total = 0;
+        for(double p : percentList){
+            total += p;
+        }
+
+        //if the calculation did not add up to 100% the remaining percentage will be distributed to every item
+        if(total < 100){
+            total = 100 - total;
+            while (total > 0){
+                int perItem = (int)(total/items);
+                if(perItem > 1){
+                    for(int i = 0; i < percentList.length; i++){
+                        percentList[i] += perItem;
+                        total -= perItem;
+                    }
+                } else {
+                    percentList[0] += total;
+                    total = 0;
+                }
+            }
+        }
+        return percentList;
     }
 
     /**
